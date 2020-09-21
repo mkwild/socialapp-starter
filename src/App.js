74088fbcd015ad531430dfddd2ecae1eaf160aa6
jessica Ribbecke <jessicaribbecke@yahoo.com>
2020-09-21 14:50:23 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,153 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
 
-class App extends React.Component {
-  render() {
-    return (
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={Home}
-        />
-        <Route
-          exact
-          path="/profile/:username"
-          component={Profile}
-        />
-        <Route
-          exact
-          path="*"
-          component={NotFound}
-        />
-      </Switch>
-    );
-  }
+
+
+
+import { FormClose, Notification } from 'grommet-icons';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  Layer,
+  ResponsiveContext,
+} from "grommet";
+import "./App.css";
+
+
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background=''
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+const JerryBox = (props) => (
+  <Box
+
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+const Columns = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify="center"
+    // background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+
+
+
+
+
+function App() {
+  
+  return (
+
+
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar className="AppBar1">
+              <div className="Heading">
+                <Heading >The Jerrys</Heading>
+                
+              </div>
+
+            </AppBar>
+
+
+            <JerryBox>
+              <div className="profile-photo">Yea I'm Jerry
+          <div className="buttons">
+                  <button id="like">Like</button>
+                  <button id="follow">Follow</button>
+                  <button id="message">Message</button>
+
+                </div>
+              </div>
+            </JerryBox>
+
+            <Columns>
+              <div className="Column1">
+                <div>
+                  <Box
+                    height="200px"
+
+                  >
+                  </Box>
+                </div>
+              </div>
+              <div className="Column2">this is column 2</div>
+            </Columns>
+
+            
+          </Box>
+        )}
+
+      </ResponsiveContext.Consumer>
+
+
+
+    </Grommet >
+
+  );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/messages/Messages.js
@@ -0,0 +1,17 @@
+
+import React, {Component} from "react";
+
+class Messages extends Component {
+    constructor(props){
+        super(props);
+
+
+        this.state = {
+            formData: {
+                createAMessage: "",
+            }
+        }
+    }
+
+   
+}
