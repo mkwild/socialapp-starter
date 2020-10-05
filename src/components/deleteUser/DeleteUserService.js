937363ea56a9521861c3f202127d39004bfd3cfc
Wolf <michael.wildnauer4481@gmail.com>
2020-10-05 13:18:38 -0400

    delete fixed omg

--- src/components/deleteUser/DeleteUserService.js
+++ src/components/deleteUser/DeleteUserService.js
@@ -4,7 +4,7 @@ import { store } from "../../redux"
 
 class DeleteUserService {
     constructor(url = 'https://socialapp-api.herokuapp.com/users/', client = axios.get()) {
-        this.url = url + "deleteUser";
+        this.url = url + "deleteUser/";
         // this.client = client;
     }
 
@@ -12,7 +12,7 @@ class DeleteUserService {
         const loginData = JSON.parse(localStorage.getItem("login"));
 
         fetch(this.url + loginData.result.username, {
-            method: "GET",
+            method: "DELETE",
             headers: { Authorization: `Bearer ${loginData.result.token}`, ...jsonHeaders },
             // body: JSON.stringify(userName)
         })
